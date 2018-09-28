ff978a71e2289e44ce3b0ae4d366a4ee4a526ea0
WPoole <wpoole94@gmail.com>
2018-09-28 13:24:48 -0400

    Initialized repo.

---
+++ src/DnsClient.java
@@ -0,0 +1,10 @@
+
+public class DnsClient {
+	
+	public static void main(String[] args) {
+		// Program must be invoked according to following format:
+		// java DnsClient [-t timeout] [-r max-retries] [-p port] [-mx|-ns] @server name
+		System.out.println("Hello world");
+	}
+
+}
