787757c57d1972b543351cd72a508d78893a1718
Fabrice <fabrice.normandin@gmail.com>
2018-09-28 16:10:28 -0400

    Added Fields, removed unneeded small files
    Signed-off-by: Fabrice <fabrice.normandin@gmail.com>

--- Lab2/src/model/Message.java
+++ Lab2/src/model/Message.java
@@ -1,18 +1,16 @@
 package model;
 
-public class Message {
-	public String header;
-	public Question question;
-	public ResourceRecord[] answer;
-	public ResourceRecord[] authority;
-	public ResourceRecord[] additional;
-	
-	public Message(){
+class Message {
+	String header;
+	QuestionEntry[] question;
+	ResourceRecord[] answer;
+	ResourceRecord[] authority;
+	ResourceRecord[] additional;
+
+	Message() {
 		// TODO
 	}
-	
-	
-	
+
 	/**This is the format of the message header:
 	 *  					           1  1  1  1  1  1  
 	 *  0   1  2  3  4  5  6  7  8  9  0  1  2  3  4  5  
@@ -30,41 +28,178 @@ public class Message {
 	 *  |                   ARCOUNT                     |  
 	 *  +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
 	 */
-	public class Header {
-		/** A 16 bit identifier assigned by the program that
-		 * generates any kind of query. This identifier is copied 
-		 * the corresponding reply and can be used by the requester 
-		 * to match up replies to outstanding queries.
+	class Header {
+		/**
+		 * A 16 bit identifier assigned by the program that generates any kind
+		 * of query. This identifier is copied the corresponding reply and can
+		 * be used by the requester to match up replies to outstanding queries.
 		 */
-		public int ID;
-		/** A one bit field that specifies whether this message is a
-		 *  query (0), or a response (1).
+		int ID;
+		/**
+		 * A one bit field that specifies whether this message is a query (0),
+		 * or a response (1).
 		 */
-		public boolean QR;
-		/** Authoritative Answer - this bit is valid in responses,
-		 * and specifies that the responding name server is an
-		 * authority for the domain name in question section.
+		boolean QR;
+		/**
+		 * A four bit field that specifies kind of query in this message. This
+		 * value is set by the originator of a query and copied into the
+		 * response. The values are:
 		 */
-		public boolean AA;	
-		public Header(){
-			// TODO		
-		}		
+		OpCode OPCODE;
+		/**
+		 * Authoritative Answer - this bit is valid in responses, and specifies
+		 * that the responding name server is an authority for the domain name
+		 * in question section.
+		 */
+		boolean AA;
+		/**
+		 * TrunCation - specifies that this message was truncated due to length
+		 * greater than that permitted on the transmission channel.
+		 */
+		boolean TC;
+		/**
+		 * Recursion Desired - this bit may be set in a query and is copied into
+		 * the response. If RD is set, it directs the name server to pursue the
+		 * query recursively. Recursive query support is optional.
+		 */
+		boolean RD;
+		/**
+		 * Recursion Available - this be is set or cleared in a response, and
+		 * denotes whether recursive query support is available in the name
+		 * server.
+		 */
+		boolean RA;
+		/**
+		 * Reserved for future use. Must be zero in all queries and responses.
+		 */
+		int Z;
+		/**
+		 * Response code - this 4 bit field is set as part of responses.
+		 */
+		ResponseCode RCODE;
+		/**
+		 * an unsigned 16 bit integer specifying the number of entries in the
+		 * question section.
+		 */
+		short QDCOUNT;
+		/**
+		 * an unsigned 16 bit integer specifying the number of resource records
+		 * in the answer section.
+		 */
+		short ANCOUNT;
+		/**
+		 * an unsigned 16 bit integer specifying the number of name server
+		 * resource records in the authority records section.
+		 */
+		short NSCOUNT;
+		/**
+		 * an unsigned 16 bit integer specifying the number of resource records
+		 * in the additional records section
+		 */
+		short ARCOUNT;
+
+		Header() {
+			// TODO
+		}
 	}
 
 	/** Enum for the Message Header's OPCODE field. */
-	enum OPCODE {
-		/**a standard query*/
+	enum OpCode {
+		/** a standard query */
 		QUERY,
-		/**an inverse query*/
+		/** an inverse query */
 		IQUERY,
-		/**a server status request*/
-		STATUS,
-		RESERVED;
+		/** a server status request */
+		STATUS, RESERVED;
+	}
+
+	enum ResponseCode {
+		/** No error condition */
+		NO_ERROR,
+		/**
+		 * The name server was unable to interpret the query
+		 */
+		FORMAT_ERROR,
+		/**
+		 * The name server was unable to process this query due to a problem
+		 * with the name server.
+		 */
+		SERVER_FAILURE,
+		/**
+		 * Meaningful only for responses from an authoritative name server, this
+		 * code signifies that the domain name referenced in the query does not
+		 * exist.
+		 */
+		NAME_ERROR,
+		/**
+		 * The name server does not support the requested kind of query.
+		 */
+		NOT_IMPLEMENTED,
+		/**
+		 * The name server refuses to perform the specified operation for policy
+		 * reasons. For example, a name server may not wish to provide the
+		 * information to the particular requester, or a name server may not
+		 * wish to perform a particular operation (e.g., zone transfer) for
+		 * particular data.
+		 */
+		REFUSED,
+	}
+
+	class QuestionEntry {
+		/**
+		 * a domain name represented as a sequence of labels, where each label
+		 * consists of a length octet followed by that number of octets. The
+		 * domain name terminates with the zero length octet for the null label
+		 * of the root. Note that this field may be an odd number of octets; no
+		 * padding is used.
+		 */
+		byte[] QNAME;
+		/**
+		 * a two octet code which specifies the type of the query. The values
+		 * for this field include all codes valid for a TYPE field, together
+		 * with some more general codes which can match more than one type of
+		 * RR.
+		 */
+		QueryType QTYPE;
+		/**
+		 * a two octet code that specifies the class of the query. For example,
+		 * the QCLASS field is IN for the Internet.
+		 */
+		QueryClass qClass;
 	}
 	
-	public class Question{
-		QType queryType;
-		QClass queryClass;
-		String queryDomainName;
+	public enum QueryType {
+		/** A request for a transfer of an entire zone */
+		AXFR (252),
+		/** A request for mailbox-related records (MB, MG or MR) */
+		MAILB (253),
+		/** A request for mail agent RRs (Obsolete - see MX) */
+		MAILA (254),
+		/** ('*') 255 A request for all records */
+		ALL (255);
+		public final int value;
+		private QueryType(int value){
+			this.value = value;
+		}
+	}
+	public enum QueryClass {
+		/** the Internet*/
+		IN (1),
+		/** the CSNET class (Obsolete - used only for examples in
+		some obsolete RFCs) */
+		CS (2),
+		/**
+		 * the CHAOS class
+		 */
+		CH (3),
+		/**
+		 * Hesiod [Dyer 87]
+		 */
+		HS (4),
+		ALL (255);
+		public final int value;
+		private QueryClass(int value){
+			this.value = value;
+		}
 	}
 }
--- Lab2/src/model/ResourceRecord.java
+++ Lab2/src/model/ResourceRecord.java
@@ -1,81 +1,85 @@
 package model;
 
+class ResourceRecord {
+	/**
+	 * An owner name, i.e., the name of the node to which this resource record
+	 * pertains. ("a domain name is terminated by a length byte of zero. The
+	 * high order two bits of every length octet must be zero, and the remaining
+	 * six bits of the length field limit the label to 63 octets or less."
+	 */
+	byte[] NAME;
+	/**
+	 * two octets containing one of the RR type codes. This field specifies the
+	 * meaning of the data in the RDATA field.
+	 */
+	RRType TYPE;
+	/**
+	 * two octets which specify the class of the data in the RDATA field.
+	 */
+	RRClass CLASS;
+	/**
+	 * a 32 bit unsigned integer that specifies the time interval (in seconds)
+	 * that the resource record may be cached before it should be discarded.
+	 * Zero values are interpreted to mean that the RR can only be used for the
+	 * transaction in progress, and should not be cached.
+	 */
+	int TTL;
+	/**
+	 * an unsigned 16 bit integer that specifies the length in octets of the
+	 * RDATA field.
+	 */
+	char RDLENGTH;
+	/**
+	 * a variable length string of octets that describes the resource. The
+	 * format of this information varies according to the TYPE and CLASS of the
+	 * resource record. For example, the if the TYPE is A and the CLASS is IN,
+	 * the RDATA field is a 4 octet ARPA Internet address.
+	 */
+	byte[] RDATA;
 
+	enum RRClass {
 
-public class ResourceRecord {
-	public enum RRClass {
-		bobo;
 	}
+
 	public enum RRType {
 		/** a host address */
-		A	(1),
+		A(1),
 		/** an authoritative name server */
-		NS	(2),
-		/** a mail destination (Obsolete - use MX)*/
-		MD 	(3),
-		/** a mail forwarder (Obsolete - use MX)*/
-		MF	(4),
-		/** the canonical name for an alias*/
-		CNAME 	(5),
-		/** marks the start of a zone of authority*/
-		SOA (6),
-		/** a mailbox domain name (EXPERIMENTAL)*/
-		MB (7),
-		/** a mail group member (EXPERIMENTAL)*/
-		MG	(8),
-		/** a mail rename domain name (EXPERIMENTAL)*/
-		MR	(9),
-		/** a null RR (EXPERIMENTAL)*/
-		NULL	(10),
-		/** a well known service description*/
-		WKS	(11),
-		/** a domain name pointer*/
-		PTR	(12),
-		/** host information*/
-		HINFO	(13),
-		/** mailbox or mail list information*/
-		MINFO	(14),
-		/** mail exchange*/
-		MX	(15),
-		/** text strings*/
-		TXT	(16);
-		
+		NS(2),
+		/** a mail destination (Obsolete - use MX) */
+		MD(3),
+		/** a mail forwarder (Obsolete - use MX) */
+		MF(4),
+		/** the canonical name for an alias */
+		CNAME(5),
+		/** marks the start of a zone of authority */
+		SOA(6),
+		/** a mailbox domain name (EXPERIMENTAL) */
+		MB(7),
+		/** a mail group member (EXPERIMENTAL) */
+		MG(8),
+		/** a mail rename domain name (EXPERIMENTAL) */
+		MR(9),
+		/** a null RR (EXPERIMENTAL) */
+		NULL(10),
+		/** a well known service description */
+		WKS(11),
+		/** a domain name pointer */
+		PTR(12),
+		/** host information */
+		HINFO(13),
+		/** mailbox or mail list information */
+		MINFO(14),
+		/** mail exchange */
+		MX(15),
+		/** text strings */
+		TXT(16);
+
 		public final int value;
-		private RRType(int value){
+
+		private RRType(int value) {
 			this.value = value;
 		}
 	}
-	
-	public enum QTypes {
-		/** A request for a transfer of an entire zone */
-		AXFR (252),
-		/** A request for mailbox-related records (MB, MG or MR) */
-		MAILB (253),
-		/** A request for mail agent RRs (Obsolete - see MX) */
-		MAILA (254),
-		/** ('*') 255 A request for all records */
-		STAR (255);
-		public final int value;
-		private QTypes(int value){
-			this.value = value;
-		}
-	}
-	
-	/**
-	 * An owner name, i.e., the name of the node to which this resource record pertains.
-	 * ("a domain name is terminated by a length byte of zero. The high order two bits 
-	 * of every length octet must be zero, and the remaining six bits of the length field
-	 * limit the label to 63 octets or less."
-	 */
-	public byte[] NAME;
-	public RRType TYPE;
-	public RRClass CLASS;
-	public int TTL;
-	public char RDLENGTH;
-	/**
-	 * a variable length string of octets that describes the resource.
-	 * The format of this information varies according to the TYPE and CLASS of the resource record.
-	 */
-	public byte[] RDATA;
-	
+
 }
